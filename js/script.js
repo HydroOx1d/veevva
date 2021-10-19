63334e03ebf9d7cb48e68d8363c289e326c6dfcd
anvar <turdaliev.anvar11@gmail.com>
2021-10-20 00:22:08 +0300

    perfectPixel

--- js/script.js
+++ js/script.js
@@ -43,12 +43,12 @@ for (let i = 0; i < backUp.length; i++) {
     }
     navigation.removeChild(menuBgc);
 
-    navItem.forEach(el => {
-      el.classList.remove('active3')
-    })
-    navImg.forEach(el => {
-      el.style.filter = 'contrast(0)'
-    })
+    navItem.forEach((el) => {
+      el.classList.remove("active3");
+    });
+    navImg.forEach((el) => {
+      el.style.filter = "contrast(0)";
+    });
   });
 }
 
@@ -57,12 +57,12 @@ menuBgc.onclick = function () {
     el.classList.remove("active2");
   });
   navigation.removeChild(menuBgc);
-  navItem.forEach(el => {
-    el.classList.remove('active3')
-  })
-  navImg.forEach(el => {
-    el.style.filter = 'contrast(0)'
-  })
+  navItem.forEach((el) => {
+    el.classList.remove("active3");
+  });
+  navImg.forEach((el) => {
+    el.style.filter = "contrast(0)";
+  });
 };
 
 bgc.onclick = function () {
@@ -72,13 +72,13 @@ bgc.onclick = function () {
   }
   head.removeChild(bgc);
   document.body.style.overflow = "auto";
-  navigation.removeChild(menuBgc)
-  navItem.forEach(el => {
-    el.classList.remove('active3')
-  })
-  navImg.forEach(el => {
-    el.style.filter = 'contrast(0)'
-  })
+  navigation.removeChild(menuBgc);
+  navItem.forEach((el) => {
+    el.classList.remove("active3");
+  });
+  navImg.forEach((el) => {
+    el.style.filter = "contrast(0)";
+  });
 };
 
 closeMenu.addEventListener("click", () => {
@@ -86,12 +86,12 @@ closeMenu.addEventListener("click", () => {
   document.body.style.overflow = "auto";
   head.removeChild(bgc);
 
-  navItem.forEach(el => {
-    el.classList.remove('active3')
-  })
-  navImg.forEach(el => {
-    el.style.filter = 'contrast(0)'
-  })
+  navItem.forEach((el) => {
+    el.classList.remove("active3");
+  });
+  navImg.forEach((el) => {
+    el.style.filter = "contrast(0)";
+  });
 });
 
 for (let i = 0; i < navItem.length; i++) {
@@ -101,18 +101,18 @@ for (let i = 0; i < navItem.length; i++) {
     navigation.appendChild(menuBgc);
     if (content.classList.contains("active2")) {
       content.classList.remove("active2");
-      navItem[i].classList.remove('active3')
-      navigation.removeChild(menuBgc)
-      navImg.forEach(el => {
-        el.style.filter = 'contrast(0)'
-      })
+      navItem[i].classList.remove("active3");
+      navigation.removeChild(menuBgc);
+      navImg.forEach((el) => {
+        el.style.filter = "contrast(0)";
+      });
     } else {
       content2.classList.remove("active2");
       content.classList.add("active2");
-      navItem[i].classList.add("active3")
-      navImg.forEach(el => {
-        el.style.filter = 'contrast(10)'
-      })
+      navItem[i].classList.add("active3");
+      navImg.forEach((el) => {
+        el.style.filter = "contrast(10)";
+      });
     }
   });
 }
@@ -127,7 +127,8 @@ stories.forEach((store) => {
     const modal = document.createElement("div");
     modal.classList.add("modal");
     modal.innerHTML = ` <button class='close'><i class='fas fa-times'></i></button> 
-    <button class='next'>next</button><button class='prev'>prev</button>
+    <button class='next-st'> <img src="./img/caruselIcon/next.svg" alt=""></button>
+    <button class='prev-st'> <img src="./img/caruselIcon/leftDirection.svg" alt=""> </button>
     <div class="testomonial-container">
 <div class="progress-bar"></div>
 <p class="testomonial">
@@ -164,8 +165,8 @@ stories.forEach((store) => {
     const role = modal.querySelector(".role");
     const progressB = modal.querySelector(".progress-bar");
 
-    const next = modal.querySelector(".next");
-    const prev = modal.querySelector(".prev");
+    const next = modal.querySelector(".next-st");
+    const prev = modal.querySelector(".prev-st");
 
     const testomonials = [
       {
@@ -380,51 +381,6 @@ function goToSlide(slideNumber) {
 }
 
 // CAROUSEL------------------------------------------------------------------------
-// $(".logo-slider").slick({
-//   slidesToShow: 9,
-//   slidesToScroll: 1,
-//   arrows: true,
-//   autoplay: true,
-//   autoplaySpeed: 2000,
-//   infinity: true,
-//   responsive: [
-//     {
-//       breakpoint: 1094,
-//       settings: {
-//         slidesToShow: 6,
-//         slidesToScroll: 3,
-//         infinite: true,
-//       },
-//     },
-//     {
-//       breakpoint: 800,
-//       settings: {
-//         slidesToShow: 4,
-//         slidesToScroll: 2,
-//         infinite: true,
-//       },
-//     },
-//     {
-//       breakpoint: 600,
-//       settings: {
-//         rows: 2,
-//         slidesToShow: 3,
-//         slidesToScroll: 1,
-//         infinite: true,
-//       },
-//     },
-
-//     {
-//       breakpoint: 400,
-//       settings: {
-//         rows:2,
-//         slidesToShow: 2,
-//         slidesToScroll: 3,
-//         infinite: true,
-//       },
-//     },
-//   ],
-// });
 
 $("#slick1").slick({
   button: true,
@@ -449,6 +405,14 @@ $("#slick1").slick({
     },
   ],
 });
+$("#stories-container").slick({
+  slidesToShow: 14,
+  slidesToScroll: 2,
+  arrows: true,
+  button: false,
+  variableWidth: true,
+  infinity: true,
+});
 
 // CARDS
 const likeImg = document.querySelectorAll(".like-img");
@@ -457,18 +421,18 @@ likeImg.forEach((like) => {
   like.addEventListener("mouseover", () => {
     like.src = "./img/cardImg/like.png";
   });
-  like.addEventListener("mouseout",()=>{
-    like.src = "./img/cardImg/likeIcon.svg"
-  })
+  like.addEventListener("mouseout", () => {
+    like.src = "./img/cardImg/likeIcon.svg";
+  });
 });
 
-const likeImgBlack = document.querySelectorAll('.like-img-black')
+const likeImgBlack = document.querySelectorAll(".like-img-black");
 
-likeImgBlack.forEach((likeBlack)=>{
-likeBlack.addEventListener('mouseover', ()=>{
-  likeBlack.src = "./img/cardImg/like.png"
-})
-likeBlack.addEventListener('mouseout', ()=>{
-  likeBlack.src = "./img/bestsellerImg/blackHeard.png"
-})
-})
+likeImgBlack.forEach((likeBlack) => {
+  likeBlack.addEventListener("mouseover", () => {
+    likeBlack.src = "./img/cardImg/like.png";
+  });
+  likeBlack.addEventListener("mouseout", () => {
+    likeBlack.src = "./img/bestsellerImg/blackHeard.png";
+  });
+});
